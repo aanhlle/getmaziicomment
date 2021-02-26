4d78f4ccfcce8f9db3c88912cf8134b92e8387de
aanhlle <levietanh1112@gmail.com>
2021-02-26 20:58:39 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# cách sử dụng tool
+1. Mở cmd hoặc terminal thư mục
+2. npm install - cài đặt thư viện node modules
+3. node index [file_input_name] [file_output_name] - chạy file script
+4. chờ đợi đến khi xuất hiện "COMPLETED" ( có thể ấn tổ hợp CTRL + C để dừng file script)
+
+
+# Notes:
+## Để nguyên máy khi đang chạy file script
+## không mở file excel khi đang chạy file script
+## Format file excel đầu vào phải giống file excel mẫu
+## file excel đầu ra phải tạo sẵn và là file excel rỗng
+
+# chạy demo:
+## node index kanji output
---
+++ index.js
@@ -0,0 +1,84 @@
+const puppeteer = require('puppeteer');
+const params = process.argv.slice(2);
+const inputFileName = params[0];
+const outputFileName = params[1];
+const XLSX = require('xlsx');
+const Excel = require('exceljs');
+
+async function getCommentsOfKanji(URL, listKanji) {
+  try {
+    const browser = await puppeteer.launch();
+    const pdfs = listKanji.map(async (k, i) => {
+      const page = await browser.newPage();
+      await page.goto(`${URL}${k}`, {
+        waitUntil: 'networkidle2',
+        timeout: 120000,
+      });
+      let content = await page.evaluate(async () => {
+        let comments = document.querySelectorAll('p.mean');
+        if (comments.length < 1) return 'Không có nghĩa';
+        comments = [...comments][0];
+        return comments.innerHTML.trim();
+      });
+      await page.close();
+      return {
+        kanji: k,
+        comment: content
+      };
+    });
+
+    return Promise.all(pdfs).then((result) => {
+      browser.close();
+      return result;
+    });
+  } catch (err) {
+    console.log("err", err);
+  }
+}
+
+async function doCrawl(data) {
+  const URL = 'https://mazii.net/#!/search?dict=javi&type=k&query=';
+  let kanji = [];
+  let count = 0;
+  while (data.length > 0) {
+    kanji = data.splice(0, 10).map(k => k['Kanji']);
+    let num = count * 10;
+    let eachRs = await getCommentsOfKanji(URL, kanji);
+    await appendToExcel(eachRs,num);
+    count++;
+  }
+  console.log("======= COMPLETED ======")
+}
+
+
+
+
+function appendToExcel(each10Rs,num) {
+  const workbook = new Excel.Workbook();
+  return workbook.xlsx.readFile(`${outputFileName}.xlsx`)
+    .then(function (data) {
+      var worksheet = workbook.getWorksheet(1);
+      each10Rs.forEach((rs,index) => {
+        let row = worksheet.getRow((num + index + 1));
+        row.getCell(1).value = rs.kanji;
+        row.getCell(2).value = rs.comment;
+        row.commit();
+      })
+      console.log("DONE in " + ((num/10)+1).toString());
+      return workbook.xlsx.writeFile(`${outputFileName}.xlsx`);
+    })
+  }
+
+
+  function main() {
+    console.log("======= START ======")
+    const wb = XLSX.readFile(`${inputFileName}.xlsx`);
+    const sheet_name_list = wb.SheetNames;
+    const data = XLSX.utils.sheet_to_json(wb.Sheets[sheet_name_list[0]]);
+    doCrawl(data);
+  }
+
+
+
+
+  main();
